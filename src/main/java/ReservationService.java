407ee841ae163bc2b4bd13238de2e784a7024d74
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-11-08 21:38:30 +0900

    feat : 할인 요금 계산 메서드 생성

--- src/main/java/ReservationService.java
+++ src/main/java/ReservationService.java
@@ -1,6 +1,21 @@
+import domain.Screening;
+
 public class ReservationService {
 
-    public Reservation reserveScreening(){
+    public Reservation reserveScreening(Long customerId, Long screeningId, Long seatId, Payment payment){
+        Screening screening;
 
+        int fee = calcuateFee();
+    }
+
+    public int calcuateFee(){
+        int originalFee = seat.getPrice();
+        int fee = originalFee;
+        if(screening.isMovieDay()){
+            fee = (int)(originalFee * 0.9);
+        }
+        if(screening.isSatisfiedTimeCondition()){
+            fee -= 2000;
+        }
     }
 }
