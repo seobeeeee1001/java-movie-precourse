2a5a91cdedf0421ec5c5b513ecaac6daf90e06f8
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-11-09 00:59:31 +0900

    feat : 결제 금액 계산 및 예약 생성 구현

--- src/main/java/ReservationService.java
+++ src/main/java/ReservationService.java
@@ -1,14 +1,36 @@
+import domain.Reservation;
 import domain.Screening;
+import domain.Seat;
+import domain.User;
+import persistence.ReservationDao;
+import persistence.ScreeningDao;
+import persistence.SeatDao;
+import persistence.UserDao;
 
 public class ReservationService {
+    private ScreeningDao screeningDao;
+    private SeatDao seatDao;
+    private UserDao userDao;
+    private ReservationDao reservationDao;
 
-    public Reservation reserveScreening(Long customerId, Long screeningId, Long seatId, Payment payment){
-        Screening screening;
-
-        int fee = calcuateFee();
+    public ReservationService(ScreeningDao screeningDao, SeatDao seatDao, UserDao userDao) {
+        this.screeningDao = screeningDao;
+        this.seatDao = seatDao;
+        this.userDao = userDao;
     }
 
-    public int calcuateFee(){
+    public Reservation reserveScreening(Long customerId, Long screeningId, Long seatId, String payment){
+        Screening screening = screeningDao.getScreening(screeningId);
+        Seat seat = seatDao.getSeat(seatId);
+        User user = userDao.getUser(customerId);
+        int fee = calcuateFee(screening, seat, user, payment);
+        Reservation reservation = makeReservation(screening,seat,user,fee);
+        reservationDao.insert(reservation);
+
+        return reservation;
+    }
+
+    public int calcuateFee(Screening screening, Seat seat, User user, String payment){
         int originalFee = seat.getPrice();
         int fee = originalFee;
         if(screening.isMovieDay()){
@@ -17,5 +39,17 @@ public class ReservationService {
         if(screening.isSatisfiedTimeCondition()){
             fee -= 2000;
         }
+        fee -= user.getPoint();
+        if(payment.equals("Cash")){
+            fee = (int)(fee * 0.95);
+        }
+        else if(payment.equals("Card")){
+            fee = (int)(fee * 0.98);
+        }
+        return fee;
+    }
+
+    public Reservation makeReservation(Screening screening, Seat seat, User user,int fee){
+        return new Reservation(screening.getId(),seat.getId(),user.getId(),fee);
     }
 }
